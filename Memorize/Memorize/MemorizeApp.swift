af744159f0c07dfed10ac504dbf50ca3c377f320
dgaguiar <daiannegaguiar@gmail.com>
2024-02-16 13:10:56 -0300

    init project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daianne Aguiar on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
